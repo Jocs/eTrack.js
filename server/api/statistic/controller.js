f9faf42dd77b3f3efc14eadfc73dfeb34810f027
冉 罗 <ran.luo@shuyun.com>
2016-05-09 20:50:45 +0800

    修复后端获取统计信息失败bug，原因：当数据少于两天时候，根本就没有昨天的数据

--- server/api/statistic/controller.js
+++ server/api/statistic/controller.js
@@ -8,20 +8,22 @@ export const getStatisticData = (req, res) => {
 	const { appId } = req.params
 	Statistic.findOne({appId})
 		.then(data => {
-			const { errorPerDay } = data
-			const totalDay = data.errorPerDay.length
-			const total = data.errorPerDay.reduce((acc, item) => acc + item.js + item.ajax, 0)
-			const yesterdayJs = data.errorPerDay[totalDay - 2].js
-			const beforeYesterdayJs = data.errorPerDay[totalDay - 3].js
-			const yesterdayJsCompare = beforeYesterdayJs === 0
-				? null : (yesterdayJs - beforeYesterdayJs) / beforeYesterdayJs
 
-			const yesterdayAjax = data.errorPerDay[totalDay - 2].ajax
-			const beforeYesterdayAjax = data.errorPerDay[totalDay - 3].ajax
-			const yesterdayAjaxCompare = beforeYesterdayAjax === 0
-				? null : (yesterdayAjax - beforeYesterdayAjax) / beforeYesterdayAjax
 			if (data === null) res.send({code: 2})
 			else {
+				const { errorPerDay } = data
+				const totalDay = data.errorPerDay.length
+				const total = data.errorPerDay.reduce((acc, item) => acc + item.js + item.ajax, 0)
+				const yesterdayJs = totalDay >= 2 ? data.errorPerDay[totalDay - 2].js : 0
+				const beforeYesterdayJs = totalDay >= 3 ? data.errorPerDay[totalDay - 3].js : 0
+				const yesterdayJsCompare = beforeYesterdayJs === 0
+					? null : (yesterdayJs - beforeYesterdayJs) / beforeYesterdayJs
+
+				const yesterdayAjax = totalDay >= 2 ? data.errorPerDay[totalDay - 2].ajax : 0
+				const beforeYesterdayAjax = totalDay >= 3 ? data.errorPerDay[totalDay - 3].ajax : 0
+				const yesterdayAjaxCompare = beforeYesterdayAjax === 0
+					? null : (yesterdayAjax - beforeYesterdayAjax) / beforeYesterdayAjax
+
 				res.send({
 					code: 1,
 					total,
@@ -35,6 +37,7 @@ export const getStatisticData = (req, res) => {
 			}
 		})
 		.catch(err => {
+			console.log(err)
 			res.send({code: 0, err})
 		})
 }
