2901714fc30b70c74ccb45515b061c3e3bf2f7b5
冉 罗 <ran.luo@shuyun.com>
2016-05-13 00:55:58 +0800

    后端查询功能完成

--- server/api/error/controller.js
+++ server/api/error/controller.js
@@ -169,3 +169,96 @@ export const errorsWithLocation = (req, res) => {
 		})
 }
 
+export const simpleSearch = (req, res) => {
+	const { appId, pageNumber, pageSize, message } = req.params
+	Error
+	.find({appId})
+	.sort({'time': -1})
+	.where({'message': {$regex: new RegExp(message, 'i')}})
+	.skip((Number(pageNumber) - 1) * Number(pageSize))
+	.limit(Number(pageSize))
+	.populate({
+		path: 'environment',
+		select: 'url'
+	})
+	.populate({
+		path: 'userAgentInfo',
+		select: 'browser'
+	})
+	.select('environment userAgentInfo message errorType time user')
+	.then(data => {
+		return Error.find({appId}).count({'message': {$regex: new RegExp(message, 'i')}})
+		.then(total => {
+			console.log(total)
+			res.send({code: 1, data, pageSize, pageNumber, total: Math.ceil(total / pageSize)})
+		})
+	})
+	.catch(err => {
+		res.send({code: 0, err})
+	})
+}
+
+export const complexSearch = (req, res) => {
+	const {
+		appId,
+		pageNumber,
+		pageSize,
+		include,
+		errorType,
+		startTime,
+		endTime,
+		browser,
+		user
+	} = req.body
+
+
+	const IN_REGEXP = include !== '' ? new RegExp(include, 'i') : new RegExp(/.*/, 'i')
+	const USER_REGEXP = user !== '' ? new RegExp(user, 'i') : new RegExp(/.*/, 'i')
+	const Error_TYPE_REGEXP = errorType === 'all' ? new RegExp(/.*/, 'i') : errorType === 'ajax' ? new RegExp('ajax', 'i') : new RegExp('^((?!ajax).)*$', 'i')
+	const start = new Date(Number(startTime))
+	const end = new Date(Number(endTime))
+	const BROWSER_REGEXP = browser === 'all' ? new RegExp(/.*/, 'i') : new RegExp(browser, 'i')
+
+	console.log(start)
+
+	Error
+	.find({appId})
+	.where({message: {$regex: IN_REGEXP}})
+	.where({errorType: {$regex: Error_TYPE_REGEXP}})
+	.where({createdAt: {$gt: start, $lt: end}})
+	.where({user: {$regex: USER_REGEXP}})
+	.skip((Number(pageNumber) - 1) * Number(pageSize))
+	.limit(Number(pageSize))
+	.populate({
+		path: 'environment',
+		select: 'url'
+	})
+	.populate({
+		path: 'userAgentInfo',
+		select: 'browser',
+		match: {browser: new RegExp(BROWSER_REGEXP, 'i')}
+	})
+	.select('environment userAgentInfo message errorType time user')
+	.then(data => {
+		console.log(data)
+		Error
+		.find({appId})
+		.where({message: {$regex: IN_REGEXP}})
+		.where({errorType: {$regex: Error_TYPE_REGEXP}})
+		.where({createdAt: {$gt: start, $lt: end}})
+		.where({user: {$regex: USER_REGEXP}})
+		.populate({
+			path: 'userAgentInfo',
+			select: 'browser',
+			match: {browser: new RegExp(BROWSER_REGEXP, 'i')}
+		})
+		.count()
+		.then(total => {
+			console.log(total, 'total')
+			res.send({code: 1, data, total: Math.ceil(total / pageSize)})
+		})
+	})
+	.catch(err => {
+		res.send({code: 0, err})
+	})
+}
--- server/api/error/index.js
+++ server/api/error/index.js
@@ -3,7 +3,15 @@
  */
 
 import express from 'express'
-import { receiveFault, receiveError, getError, singleError, errorsWithLocation } from './controller'
+import {
+	receiveFault,
+	receiveError,
+	getError,
+	singleError,
+	errorsWithLocation,
+	simpleSearch,
+	complexSearch
+} from './controller'
 import { initBrowser } from './browser'
 import { initStatistic } from './statistic'
 
@@ -16,6 +24,8 @@ router.post('/capture', receiveError)
 router.post('/fault', receiveFault)
 router.get('/singleError/:errorId', singleError)
 router.get('/errorsWithLocation/:appId', errorsWithLocation)
+router.get('/simpleSearch/:appId/:pageNumber/:pageSize/:message', simpleSearch)
+router.post('/complexSearch', complexSearch)
 router.get('/errors/:appId/:pageNumber/:pageSize', getError)
 
 
