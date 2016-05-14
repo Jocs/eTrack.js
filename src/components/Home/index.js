dbd1e4602ccc5a34fc2079f70c7a14ed110e3b39
冉 罗 <ran.luo@shuyun.com>
2016-05-14 17:18:38 +0800

    首页动画效果

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -13,32 +13,50 @@ class Home extends Component {
 	constructor(props) {
 		super(props)
 		this.state = {
-			browsers: [
-				{ 'name': 'Chrome-52', 'value': 9 },
-				{ 'name': 'Chrome-51', 'value': 8 },
-				{ 'name': 'Chrome-49', 'value': 3 },
-				{ 'name': 'Safari-9', 'value': 6 },
-				{ 'name': 'Firefox-47', 'value': 6 },
-				{ 'name': 'Firefox-44', 'value': 5 },
-				{ 'name': 'Firefox-45', 'value': 5 },
-				{ 'name': 'Opera-36', 'value': 7 },
-				{ 'name': 'Opera-37', 'value': 3 },
-				{ 'name': 'Chrome-50', 'value': 4 },
-				{ 'name': 'Firefox-46', 'value': 2 }
-			]
+			about: 'eTrack is a JavaScript Error monitor and reportor!.  Help you build a better JavaScript Application!.',
+			showWord: '',
+			cursor: true
 		}
 	}
 
 	static propTypes = {
 		toggleLoginPanel: PropTypes.func.isRequired
 	}
+
+	componentDidMount() {
+		this.timerCursor = setInterval(() => {
+			this.setState({cursor: !this.state.cursor})
+		}, 500)
+		this.timerWord = setInterval(() => {
+			if (this.state.showWord.length < this.state.about.length) {
+				this.setState({
+					showWord: this.state.about.substr(0, this.state.showWord.length + 1)
+				})
+			} else {
+				this.setState({
+					showWord: ''
+				})
+			}
+		}, 100)
+	}
+
+	componentWillUnmount() {
+		clearInterval(this.timerCursor)
+		clearInterval(this.timerWord)
+	}
+
 	render() {
 		const { toggleLoginPanel } = this.props
+		const { cursor, showWord } = this.state
+
 		return (
 			<div className='home'>
 				<section className='logo-page'>
 					<h1>{'{eTrack.Js}'}</h1>
-					<h3>帮您构建更好的JavaScript应用</h3>
+					<h3>/*&nbsp;
+					{/\./.test(showWord) ? showWord.split(/\./)[showWord.split(/\./).length - 1] : showWord}
+					<span style={{color: cursor ? '#eee' : 'rgb(0, 188, 212)'}}>&nbsp;|</span>&nbsp;*/
+					</h3>
 					<div>
 						<a href='https://github.com/Jocs/jocs.github.io/issues/1'>阅读文档</a>
 						<a href='javascript:;'
@@ -61,11 +79,7 @@ class Home extends Component {
 						<p>让开发、测试更容易</p>
 					</div>
 					<div className='pie'>
-						<Pie
-							width={420}
-							height={400}
-							browsers={this.state.browsers}
-						/>
+						<img src='./src/assets/images/browser.jpg'/>
 					</div>
 				</section>
 				<section className='detail-page'>
